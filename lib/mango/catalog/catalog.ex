316d7685cb08be360b5a94669f5b32bb07d4f59c
EQuimper <quimperemanuel@gmail.com>
2017-11-11 13:45:21 -0500

    Add cart test

--- lib/mango/catalog/catalog.ex
+++ lib/mango/catalog/catalog.ex
@@ -2,11 +2,13 @@ defmodule Mango.Catalog do
   alias Mango.Catalog.Product
   alias Mango.Repo
 
+  def get_product!(id), do: Product |> Repo.get!(id)
+
   def list_products do
     Product
     |> Repo.all
   end
-  
+
   def list_seasonal_products do
     list_products()
     |> Enum.filter(fn(product) -> product.is_seasonal == true end)
--- test/mango_web/acceptance/cart_test.exs
+++ test/mango_web/acceptance/cart_test.exs
@@ -32,7 +32,7 @@ defmodule MangoWeb.Acceptance.CartTest do
   test "add to cart" do
     navigate_to("/")
 
-    [product | _rest] = find_all_elements(:css, ".product-thumbnail")
+    [product | _rest] = find_all_elements(:css, ".product")
 
     product_name = find_within_element(product, :name, "cart[product_name]") |> attribute_value("value")
 
@@ -44,6 +44,6 @@ defmodule MangoWeb.Acceptance.CartTest do
 
     message = find_element(:css, ".alert") |> visible_text
 
-    assert message == "Product added to cart - #{product_name}(#{pack_size}) * 2 qty"
+    assert message == "Product added to cart - #{product_name}(#{pack_size}) x 2 qty"
   end
 end
